b20ad37a1953226003dd3f4582c318eac946e621
EdrielSantos <https://github.com/EdrielSantos/ConstructionRiskAssessment.git>
2023-05-26 14:18:21 +0800

    ReportList and ReportAssessment Fragment

---
+++ app/src/main/java/com/example/constructionriskassessment/ReportListFragment.kt
@@ -0,0 +1,25 @@
+package com.example.constructionriskassessment
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater
+
+
+import android.view.View;
+import android.view.ViewGroup
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.fragment.app.Fragment
+
+class ReportListFragment: Fragment() {
+	override fun onCreateView(
+		inflater: LayoutInflater,
+		container: ViewGroup?,
+		savedInstanceState: Bundle?
+	): View? {
+		return inflater.inflate(R.layout.fragment_reportList, container, false)
+	}
+
+}
+	
+	
